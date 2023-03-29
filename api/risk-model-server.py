1229ccc8d2d0f56c0d2d17f1fb86048db1e8fc3f
Xiang Dezhi <xiangdezhi1995@gmail.com>
2023-03-29 17:46:24 +0800

    添加新模型

--- api/risk-model-server.py
+++ api/risk-model-server.py
@@ -25,9 +25,22 @@ webapi = Flask(__name__)
 # 相对路径
 model_dir = os.path.join(os.path.dirname(__file__), '../model/')
 
-# 第一版新客模型
+# NZB_card模型
 fr = open(model_dir + 'NZB_card.pkl', 'rb')
 NZB_card = pickle.load(fr)
+
+# dk_card模型
+fr = open(model_dir + 'dk_card.pkl', 'rb')
+dk_card = pickle.load(fr)
+
+# cd_card模型
+fr = open(model_dir + 'cd_card.pkl', 'rb')
+cd_card = pickle.load(fr)
+
+# ld_card模型
+fr = open(model_dir + 'ld_card.pkl', 'rb')
+ld_card = pickle.load(fr)
+
 
 @webapi.route('/')
 def index():
@@ -45,6 +58,66 @@ def api_NZB_card():
             data[x] = np.NAN
     
     my_card = NZB_card
+
+    #得分
+    score = my_card.predict(data)
+
+    time = timeit.default_timer() - start
+    
+    response = {'code': 0, 'msg': "", 'data': {'score': score[0], 'time': time}}
+    return make_response(jsonify(response), 200)
+
+@webapi.route('/api/dk_card', methods=['post'])
+def api_dk_card():
+    start = timeit.default_timer()
+    json = request.get_json()    
+
+    data = pd.DataFrame(json["data"], index=[0])
+    for x in data.columns:
+        if json["data"][x] is None:
+            data[x] = np.NAN
+    
+    my_card = dk_card
+
+    #得分
+    score = my_card.predict(data)
+
+    time = timeit.default_timer() - start
+    
+    response = {'code': 0, 'msg': "", 'data': {'score': score[0], 'time': time}}
+    return make_response(jsonify(response), 200)
+
+@webapi.route('/api/cd_card', methods=['post'])
+def api_cd_card():
+    start = timeit.default_timer()
+    json = request.get_json()    
+
+    data = pd.DataFrame(json["data"], index=[0])
+    for x in data.columns:
+        if json["data"][x] is None:
+            data[x] = np.NAN
+    
+    my_card = cd_card
+
+    #得分
+    score = my_card.predict(data)
+
+    time = timeit.default_timer() - start
+    
+    response = {'code': 0, 'msg': "", 'data': {'score': score[0], 'time': time}}
+    return make_response(jsonify(response), 200)
+
+@webapi.route('/api/ld_card', methods=['post'])
+def api_ld_card():
+    start = timeit.default_timer()
+    json = request.get_json()    
+
+    data = pd.DataFrame(json["data"], index=[0])
+    for x in data.columns:
+        if json["data"][x] is None:
+            data[x] = np.NAN
+    
+    my_card = ld_card
 
     #得分
     score = my_card.predict(data)
